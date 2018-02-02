c984dbe2d1fba1b3157e8b658d13d1d792d7c26a
saleelkarkhanis <karkhanis.saleel@gmail.com>
2018-02-02 18:16:44 +0530

    WIP Progress - Preferences Screen UI

--- SnapXEats/SnapXEats/Modules/UserPrefernce/View/UserPreferenceViewController.swift
+++ SnapXEats/SnapXEats/Modules/UserPrefernce/View/UserPreferenceViewController.swift
@@ -16,10 +16,32 @@ class UserPreferenceViewController: BaseViewController, StoryboardLoadable {
     var presenter: UserPreferencePresentation?
 
     // MARK: Lifecycle
-
+    @IBOutlet weak var sampleLabel: UILabel!
+    @IBOutlet weak var locationInfoView: UIView!
+    
+    @IBOutlet weak var distanceRadioButton: UIButton!
+    @IBOutlet weak var ratingsRadioButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        locationInfoView.addViewBorderWithColor(color: UIColor.rgba(230.0, 230.0, 230.0, 1.0), width: 1.0, side: .bottom)
+    }
+    
+    @IBAction func radioButtonSelected(sender: UIButton) {
+        
+        if sender == distanceRadioButton && sender.isSelected == false {
+            sender.isSelected = true
+            ratingsRadioButton.isSelected = false
+        }
+        
+        if sender == ratingsRadioButton && sender.isSelected == false {
+            sender.isSelected = true
+            distanceRadioButton.isSelected = false
+        }
     }
 }
 
