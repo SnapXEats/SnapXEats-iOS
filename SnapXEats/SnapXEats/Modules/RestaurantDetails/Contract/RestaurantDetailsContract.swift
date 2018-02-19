079c967960ee7b8ec3272486c4ca05c00d295903
Durgesh Trivedi <durgesh.trivedi@synerzip.com>
2018-02-19 12:33:47 +0530

    Created Boiler plate code for restaurant details.

---
+++ SnapXEats/SnapXEats/Modules/RestaurantDetails/Contract/RestaurantDetailsContract.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantDetailsContract.swift
+//  
+//
+//  Created by Durgesh Trivedi on 19/02/18.
+//  
+//
+
+import Foundation
+
+protocol RestaurantDetailsView: class, BaseView {
+    // TODO: Declare view methods
+}
+
+protocol RestaurantDetailsPresentation: class {
+    // TODO: Declare presentation methods
+}
+
+protocol RestaurantDetailsUseCase: class {
+    // TODO: Declare use case methods
+}
+
+protocol RestaurantDetailsInteractorOutput: class {
+    // TODO: Declare interactor output methods
+}
+
+protocol RestaurantDetailsWireframe: class {
+    // TODO: Declare wireframe methods
+}
---
+++ SnapXEats/SnapXEats/Modules/RestaurantDetails/Interactor/RestaurantDetailsInteractor.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantDetailsInteractor.swift
+//  
+//
+//  Created by Durgesh Trivedi on 19/02/18.
+//  
+//
+
+import Foundation
+
+class RestaurantDetailsInteractor {
+
+    // MARK: Properties
+
+    weak var output: RestaurantDetailsInteractorOutput?
+}
+
+extension RestaurantDetailsInteractor: RestaurantDetailsUseCase {
+    // TODO: Implement use case methods
+}
