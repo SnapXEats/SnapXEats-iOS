58bc1c57c792f4efa71c81e03b74338084b0639b
Priyanka Gaikwad <priyanka.gaikwad@synerzip.com>
2018-04-17 19:01:21 +0530

    Added UI for Food Journey with outlets

---
+++ SnapXEats/SnapXEats/Modules/FoodJourney/View/FoodJourneyCells/RecentFoodJourneyPicturesCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RecentFoodJourneyPicturesCollectionViewCell.swift
+//  SnapXEats
+//
+//  Created by Priyanka Gaikwad on 17/04/18.
+//  Copyright © 2018 SnapXEats. All rights reserved.
+//
+
+import UIKit
+
+class RecentFoodJourneyPicturesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
